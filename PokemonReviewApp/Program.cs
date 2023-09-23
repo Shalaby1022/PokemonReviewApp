89dde7919c95cb4cb083aeed4692b028607a3674
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-09-23 17:28:07 +0300

    Registering Services in Startup and Creating the connection string

--- PokemonReviewApp/Program.cs
+++ PokemonReviewApp/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PokemonReviewApp.Data;
+using PokemonReviewApp.Migrations;
+
 namespace PokemonReviewApp
 {
     public class Program
@@ -6,15 +11,41 @@ namespace PokemonReviewApp
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
 
+            //registering the DB connectiviety using EF core.
+            builder.Services.AddDbContext<PokemonDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+            });
+
+
+            // Add services to the container.
+            builder.Services.AddTransient<Seeding>();
             builder.Services.AddControllers();
+        
+
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
+
+            if (args.Length == 1 && args[0].ToLower() == "seeddata")
+                SeedData(app);
+
+            void SeedData(IHost app)
+            {
+                var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+                using (var scope = scopedFactory.CreateScope())
+                {
+                    var service = scope.ServiceProvider.GetService<Seeding>();
+                    service.SeedDataContext();
+                }
+            }
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
@@ -23,11 +54,17 @@ namespace PokemonReviewApp
             }
 
             app.UseHttpsRedirection();
-
+            app.UseAuthentication();
+            app.UseRouting();
             app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
 
 
-            app.MapControllers();
+
+           
 
             app.Run();
         }
