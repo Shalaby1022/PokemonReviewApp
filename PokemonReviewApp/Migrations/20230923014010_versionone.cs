b8884daffbdbf9ddb9b91ea26de026c5a8abc783
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-09-23 17:24:31 +0300

    Adding migration and updating database

---
+++ PokemonReviewApp/Migrations/20230923014010_versionone.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokemonReviewApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class versionone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pokemons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pokemons", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reviwers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviwers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Owners",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Gym = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CountryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Owners", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Owners_Countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Countries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PokemonCategory",
+                columns: table => new
+                {
+                    PokemonId = table.Column<int>(type: "int", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PokemonCategory", x => new { x.PokemonId, x.CategoryId });
+                    table.ForeignKey(
+                        name: "FK_PokemonCategory_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PokemonCategory_Pokemons_PokemonId",
+                        column: x => x.PokemonId,
+                        principalTable: "Pokemons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Rating = table.Column<int>(type: "int", nullable: false),
+                    ReviewerId = table.Column<int>(type: "int", nullable: false),
+                    PokemonId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reviews_Pokemons_PokemonId",
+                        column: x => x.PokemonId,
+                        principalTable: "Pokemons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reviews_Reviwers_ReviewerId",
+                        column: x => x.ReviewerId,
+                        principalTable: "Reviwers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PokemonOwner",
+                columns: table => new
+                {
+                    PokemonId = table.Column<int>(type: "int", nullable: false),
+                    OwnerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PokemonOwner", x => new { x.PokemonId, x.OwnerId });
+                    table.ForeignKey(
+                        name: "FK_PokemonOwner_Owners_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "Owners",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PokemonOwner_Pokemons_PokemonId",
+                        column: x => x.PokemonId,
+                        principalTable: "Pokemons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Owners_CountryId",
+                table: "Owners",
+                column: "CountryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PokemonCategory_CategoryId",
+                table: "PokemonCategory",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PokemonOwner_OwnerId",
+                table: "PokemonOwner",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_PokemonId",
+                table: "Reviews",
+                column: "PokemonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_ReviewerId",
+                table: "Reviews",
+                column: "ReviewerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PokemonCategory");
+
+            migrationBuilder.DropTable(
+                name: "PokemonOwner");
+
+            migrationBuilder.DropTable(
+                name: "Reviews");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Owners");
+
+            migrationBuilder.DropTable(
+                name: "Pokemons");
+
+            migrationBuilder.DropTable(
+                name: "Reviwers");
+
+            migrationBuilder.DropTable(
+                name: "Countries");
+        }
+    }
+}
---
+++ PokemonReviewApp/Migrations/20230923014559_versiontwo.cs
@@ -0,0 +1,186 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokemonReviewApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class versiontwo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PokemonCategory_Categories_CategoryId",
+                table: "PokemonCategory");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PokemonCategory_Pokemons_PokemonId",
+                table: "PokemonCategory");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PokemonOwner_Owners_OwnerId",
+                table: "PokemonOwner");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PokemonOwner_Pokemons_PokemonId",
+                table: "PokemonOwner");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PokemonOwner",
+                table: "PokemonOwner");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PokemonCategory",
+                table: "PokemonCategory");
+
+            migrationBuilder.RenameTable(
+                name: "PokemonOwner",
+                newName: "PokemonOwners");
+
+            migrationBuilder.RenameTable(
+                name: "PokemonCategory",
+                newName: "pokemonCategories");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_PokemonOwner_OwnerId",
+                table: "PokemonOwners",
+                newName: "IX_PokemonOwners_OwnerId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_PokemonCategory_CategoryId",
+                table: "pokemonCategories",
+                newName: "IX_pokemonCategories_CategoryId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PokemonOwners",
+                table: "PokemonOwners",
+                columns: new[] { "PokemonId", "OwnerId" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_pokemonCategories",
+                table: "pokemonCategories",
+                columns: new[] { "PokemonId", "CategoryId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_pokemonCategories_Categories_CategoryId",
+                table: "pokemonCategories",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_pokemonCategories_Pokemons_PokemonId",
+                table: "pokemonCategories",
+                column: "PokemonId",
+                principalTable: "Pokemons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PokemonOwners_Owners_OwnerId",
+                table: "PokemonOwners",
+                column: "OwnerId",
+                principalTable: "Owners",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PokemonOwners_Pokemons_PokemonId",
+                table: "PokemonOwners",
+                column: "PokemonId",
+                principalTable: "Pokemons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_pokemonCategories_Categories_CategoryId",
+                table: "pokemonCategories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_pokemonCategories_Pokemons_PokemonId",
+                table: "pokemonCategories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PokemonOwners_Owners_OwnerId",
+                table: "PokemonOwners");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_PokemonOwners_Pokemons_PokemonId",
+                table: "PokemonOwners");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PokemonOwners",
+                table: "PokemonOwners");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_pokemonCategories",
+                table: "pokemonCategories");
+
+            migrationBuilder.RenameTable(
+                name: "PokemonOwners",
+                newName: "PokemonOwner");
+
+            migrationBuilder.RenameTable(
+                name: "pokemonCategories",
+                newName: "PokemonCategory");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_PokemonOwners_OwnerId",
+                table: "PokemonOwner",
+                newName: "IX_PokemonOwner_OwnerId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_pokemonCategories_CategoryId",
+                table: "PokemonCategory",
+                newName: "IX_PokemonCategory_CategoryId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PokemonOwner",
+                table: "PokemonOwner",
+                columns: new[] { "PokemonId", "OwnerId" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PokemonCategory",
+                table: "PokemonCategory",
+                columns: new[] { "PokemonId", "CategoryId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PokemonCategory_Categories_CategoryId",
+                table: "PokemonCategory",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PokemonCategory_Pokemons_PokemonId",
+                table: "PokemonCategory",
+                column: "PokemonId",
+                principalTable: "Pokemons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PokemonOwner_Owners_OwnerId",
+                table: "PokemonOwner",
+                column: "OwnerId",
+                principalTable: "Owners",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PokemonOwner_Pokemons_PokemonId",
+                table: "PokemonOwner",
+                column: "PokemonId",
+                principalTable: "Pokemons",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
