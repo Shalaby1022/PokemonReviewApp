16bf0c807d7afaa0f2a8c63840ebe13d7d1607cf
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-09-24 02:01:59 +0300

    Creating interface for pokemon that have necessit methods we want to implement in our repo

---
+++ PokemonReviewApp/Data/Interface/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Data.Interface
+{
+    public interface ICategoryRepository
+    {
+        ICollection<Category> GetCategories();
+        Category GetCAtegoryById(int id);
+        ICollection<Pokemon> GetPokemonByCategory(int categoryId);
+        bool CategoryExist(int catehoryId);
+
+
+    }
+}
---
+++ PokemonReviewApp/Data/Interface/IPokemonRepository.cs
@@ -0,0 +1,15 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Data.Interface
+{
+    public interface IPokemonRepository
+    {
+        ICollection<Pokemon> GetAllPokemons();
+
+        Pokemon GetPokieById(int id);
+        Pokemon GetPokieByName(string name);
+        decimal GetPokemonRating(int pokieId);
+        bool pokiemonExist(int pokemonId);
+
+    }
+}
