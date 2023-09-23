c487c1e443afab234309a4ad598edf3605f8728f
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-09-24 02:02:19 +0300

    implementing the interface funcionalities.

---
+++ PokemonReviewApp/Repository/CategoryRepository.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp.Data;
+using PokemonReviewApp.Data.Interface;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly PokemonDbContext _context;
+        public CategoryRepository(PokemonDbContext context)
+        {
+            _context = context;
+        }
+        public bool CategoryExist(int categoryId)
+        {
+            var categoryfound = _context.Categories.Any(p=>p.Id == categoryId);
+            return categoryfound;
+        }
+
+        public ICollection<Category> GetCategories()
+        {
+           var categories = _context.Categories.ToList();
+            return categories;
+        }
+
+        public Category GetCAtegoryById(int id)
+        {
+            var onecategory = _context.Categories.FirstOrDefault(p=>p.Id == id);
+            if(onecategory == null) { return null; }
+            return onecategory;
+        }
+
+        public ICollection<Pokemon> GetPokemonByCategory(int categoryId)
+        {
+            return _context.pokemonCategories.Where(p=>p.CategoryId == categoryId).Select(c=>c.Pokemon).ToList();
+        }
+    }
+}
---
+++ PokemonReviewApp/Repository/PokemonRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Data;
+using PokemonReviewApp.Data.Interface;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class PokemonRepository : IPokemonRepository
+    {
+        private readonly PokemonDbContext _context;
+
+        public PokemonRepository(PokemonDbContext context)
+        {
+            _context = context;
+        }
+        public ICollection<Pokemon> GetAllPokemons()
+        {
+            return _context.Pokemons.OrderBy(p => p.Id).ToList();
+        }
+
+        public decimal GetPokemonRating(int pokieId)
+        {
+            var reviews = _context.Reviews.Where(r => r.Id == pokieId).ToList();
+            if (reviews.Count <= 0) return 0;
+            return (decimal)reviews.Sum(r => r.Rating) / reviews.Count;
+        }
+
+        [HttpGet]
+        public Pokemon GetPokieById(int id)
+        {
+            var pokie = _context.Pokemons.FirstOrDefault(p => p.Id == id);
+            if (pokie == null)
+            {
+                return null;
+            }
+            return pokie;
+        } 
+
+        public Pokemon GetPokieByName(string name)
+        {
+            var pokiename = _context.Pokemons.FirstOrDefault(p=>p.Name == name);
+            if (pokiename == null) return null;
+            return pokiename;
+        }
+
+        public bool pokiemonExist(int pokemonId)
+        {
+            var pokiefound = _context.Pokemons.Any(p=>p.Id  == pokemonId);
+            return pokiefound;
+        }
+    }
+}
