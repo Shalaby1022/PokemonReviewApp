38ca2fd23d5fd58d9e5f5cc2c26c97f05c94bc34
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-09-24 02:01:22 +0300

    Creating controllers for respositories and using automapper in it fr DTo's

---
+++ PokemonReviewApp/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Data.Interface;
+using PokemonReviewApp.DTOs;
+
+namespace PokemonReviewApp.Controllers
+{
+    [ApiController]
+    [Route("api/Category")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryController(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public IActionResult GetAllategories()
+        {
+            var categories = _mapper.Map<IEnumerable<CategoryDto>>(_categoryRepository.GetCategories());
+            if(!ModelState.IsValid) { return BadRequest(ModelState); }
+            return Ok(categories);
+        }
+    }
+}
---
+++ PokemonReviewApp/Controllers/PokemonController.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Data.Interface;
+using PokemonReviewApp.DTOs;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Controllers
+{
+    [ApiController]
+    [Route("api/pokemon")]
+    public class PokemonController : Controller
+    {
+        private readonly IPokemonRepository _pokemonRepository;
+        private readonly IMapper _mapper;
+
+        public PokemonController(IPokemonRepository pokemonRepository , IMapper mapper )
+        {
+            _pokemonRepository = pokemonRepository;
+            _mapper = mapper;
+        }
+
+       [HttpGet]
+        public ActionResult<IEnumerable<Pokemon>> GetAllPokies()
+        {
+            var pokies =  _mapper.Map<IEnumerable<DTOs.PokemonDto>>(_pokemonRepository.GetAllPokemons());
+            if(!ModelState.IsValid) 
+            {
+                return BadRequest();
+            }
+
+            return Ok(pokies);
+        }
+
+        [HttpGet("{pokieId}")]
+        public IActionResult GetPokieById(int pokieId)
+        {
+            var pokie = _mapper.Map<PokemonDto>(_pokemonRepository.GetPokieById(pokieId));
+            if(!ModelState.IsValid && pokie == null) { return BadRequest(ModelState); }
+            return Ok(pokie);
+
+        }
+        [HttpGet("{pokieId}/rating")]
+
+        public IActionResult GetRate(int pokieId)
+        {
+            if(!_pokemonRepository.pokiemonExist(pokieId)) return NotFound();
+
+            var pokierate = _pokemonRepository.GetPokemonRating(pokieId);
+
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            return Ok(pokierate);
+            
+
+        }
+
+    }
+}
