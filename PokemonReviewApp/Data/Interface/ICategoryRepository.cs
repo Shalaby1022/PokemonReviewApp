56e5cb5b1952601b4dbcd50a0a4c08e194343c0f
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-10-07 00:20:28 +0300

    Implement search and filtering in Category interface

--- PokemonReviewApp/Data/Interface/ICategoryRepository.cs
+++ PokemonReviewApp/Data/Interface/ICategoryRepository.cs
@@ -5,7 +5,8 @@ namespace PokemonReviewApp.Data.Interface
     public interface ICategoryRepository
     {
         ICollection<Category> GetCategories();
-        Category GetCAtegoryById(int id);
+        ICollection<Category> GetAllCategories(string? name , string? SearchQuery);
+        Category GetCategoryById(int id);
         ICollection<Pokemon> GetPokemonByCategory(int categoryId);
         bool CategoryExist(int categoryId);
         bool CreateCategory (Category category);
--- PokemonReviewApp/Data/Interface/ICountryRepository.cs
+++ PokemonReviewApp/Data/Interface/ICountryRepository.cs
@@ -5,7 +5,7 @@ namespace PokemonReviewApp.Data.Interface
     public interface ICountryRepository
     {
         ICollection<Country> GetAllCountries();
-        Country GetCountry(int id);
+        Country GetCountryById(int id);
         Country GetCountryByOwner(int ownerid);
         ICollection<Owner> GetownersfromAcountry(int countryId);
         bool CountryExist(int id);
