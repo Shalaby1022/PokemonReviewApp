3ec8c151881b0a89f9eac23ece2462c587ba9694
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-10-17 01:08:48 +0300

    Add RoleDto for managing user roles

---
+++ PokemonReviewApp/DTOs/RoleDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PokemonReviewApp.DTOs
+{
+    public class RoleDto
+    {
+        [Required]
+        public string UserId { get; set; }
+        [Required]
+        public string RoleName {  get; set; }
+
+    }
+}
