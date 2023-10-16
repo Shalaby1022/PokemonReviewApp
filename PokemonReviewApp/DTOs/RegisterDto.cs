03c957983079b7ad033a6700bfbcb979830ef806
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-10-17 01:08:26 +0300

    Add RegisterDto for user registration

---
+++ PokemonReviewApp/DTOs/RegisterDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PokemonReviewApp.DTOs
+{
+    public class RegisterDto
+    {
+        [Required(ErrorMessage = "First name is required")]
+        public string FName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        public string LName { get; set; }
+
+        [Required(ErrorMessage = "Username is required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters")]
+        public string PassWord { get; set; }
+    }
+}
