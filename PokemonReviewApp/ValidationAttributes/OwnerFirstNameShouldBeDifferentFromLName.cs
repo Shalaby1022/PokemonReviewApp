1282f28bce720d3c598d0916b914ec6ba1f63f40
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-10-22 01:59:24 +0300

    class-Level input Validation with a custom Attribute

---
+++ PokemonReviewApp/ValidationAttributes/OwnerFirstNameShouldBeDifferentFromLName.cs
@@ -0,0 +1,30 @@
+﻿using PokemonReviewApp.DTOs;
+using System.ComponentModel.DataAnnotations;
+
+namespace PokemonReviewApp.ValidationAttributes
+{
+    public class OwnerFirstNameShouldBeDifferentFromLName : ValidationAttribute
+    {
+        public OwnerFirstNameShouldBeDifferentFromLName()
+        {
+        }
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if(validationContext.ObjectInstance is not OwnerDto owner)
+            {
+                throw new Exception($"Attribute" +
+                    $"{nameof(OwnerFirstNameShouldBeDifferentFromLName)}" +
+                    $"Must Be Different" +
+                    $"{nameof(OwnerDto)} or derived type"
+                    );
+            }
+
+            if(owner.FirstName == owner.LastName)
+            {
+                return new ValidationResult("First Name and Last Name Shuldn't be Equla",
+                new[] { nameof(OwnerDto) });
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
