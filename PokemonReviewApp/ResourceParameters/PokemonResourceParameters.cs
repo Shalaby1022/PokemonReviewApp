8af0c4633243fe13f6290cc44f8b43e234ae543f
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-10-23 02:21:31 +0300

    Introduce PokemonResourceParameters class for handling query parameters search , filter and paging

---
+++ PokemonReviewApp/ResourceParameters/PokemonResourceParameters.cs
@@ -0,0 +1,21 @@
+﻿namespace PokemonReviewApp.ResourceParameters
+{
+    public class PokemonResourceParameters
+    {
+        const int MaxPageSize = 20;
+        public string? name { get; set; }
+        public string? SearchQuery { get; set; }
+
+        public int PageNumber = 1;
+
+        private int _pageSize = 10;
+
+        public int PageSize
+        {
+            get=> _pageSize;
+            set=> _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
+        
+
+    }
+}
