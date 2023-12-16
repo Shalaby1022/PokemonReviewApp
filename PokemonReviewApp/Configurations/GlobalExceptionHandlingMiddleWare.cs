79ee6d1fcb18fb923c2170fa736a782bf5e03b26
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2023-12-16 14:15:23 +0200

    Implement GlobalExceptionHandlingMiddleware to catch and handle various custom exceptions, returning JSON-formatted error responses with appropriate status codes

---
+++ PokemonReviewApp/Configurations/GlobalExceptionHandlingMiddleWare.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.Services.GitHubConnector;
+using PokemonReviewApp.Exceptions;
+using System.Net;
+using System.Text.Json;
+
+namespace PokemonReviewApp.Configurations
+{
+    public class GlobalExceptionHandlingMiddleWare
+    {
+        private readonly RequestDelegate _next;
+        public GlobalExceptionHandlingMiddleWare(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Inovke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            HttpStatusCode Status;
+            var stackTrace = string.Empty;
+            var message = string.Empty;
+
+            var exceptionType = ex.GetType();
+
+            if (exceptionType == typeof(Exceptions.NotFoundException))
+            {
+                message = ex.Message;
+                Status = HttpStatusCode.NotFound;
+                stackTrace = ex.StackTrace;
+            }
+
+            else if (exceptionType == typeof(Exceptions.BadRequestException))
+            {
+                message = ex.Message;
+                Status = HttpStatusCode.BadRequest;
+                stackTrace = ex.StackTrace;
+            }
+
+            else if (exceptionType == typeof(Exceptions.NotImplementedException))
+            {
+                message = ex.Message;
+                Status = HttpStatusCode.NotImplemented;
+                stackTrace = ex.StackTrace;
+            }
+            else if (exceptionType == typeof(Exceptions.KeyNotFoundException))
+            {
+                message = ex.Message;
+                Status = HttpStatusCode.NotFound;
+                stackTrace = ex.StackTrace;
+            }
+
+            else if (exceptionType == typeof(Exceptions.UnauthorizedAccessException))
+            {
+                message = ex.Message;
+                Status = HttpStatusCode.Unauthorized;
+                stackTrace = ex.StackTrace;
+            }
+            else
+            {
+                message = ex.Message;
+                Status = HttpStatusCode.InternalServerError;
+                stackTrace = ex.StackTrace;
+            }
+
+            var exceptionResult = JsonSerializer.Serialize(new { error = message, stackTrace, });
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)Status;
+
+
+            return context.Response.WriteAsync(exceptionResult);
+
+        }
+    }
+}
